563901cab0281a275f0236725567ae27a05e1438
Nisal Dilshan <bcnisal@gmail.com>
2018-06-03 17:40:22 +0530

    User Master page added

---
+++ WebApptest1/website/user/testing.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/website/user/user.Master" AutoEventWireup="true" CodeBehind="testing.aspx.cs" Inherits="WebApptest1.website.user.testing" %>
+
+<asp:Content ID="content1" ContentPlaceHolderID="c1" runat="server">
+    this is for testing
+</asp:Content>
---
+++ WebApptest1/website/user/user.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApptest1.website.user
+{
+    public partial class user : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
