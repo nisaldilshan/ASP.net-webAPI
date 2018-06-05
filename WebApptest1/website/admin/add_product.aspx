ca20079d6a97efb693c7c078af03b98396b62741
Nisal Dilshan <bcnisal@gmail.com>
2018-06-06 00:04:48 +0530

    add data from admin side

---
+++ WebApptest1/website/admin/add_product.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/website/admin/admin.Master" AutoEventWireup="true" CodeBehind="add_product.aspx.cs" Inherits="WebApptest1.website.admin.add_product" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="c1" runat="server">
+    <h3> Add Product Page </h3>
+    <table>
+        <tr>
+        <td>product name</td>
+        <td><asp:TextBox ID="t1" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+        <td>product description</td>
+        <td><asp:TextBox ID="t2" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+        <td>product price</td>
+        <td><asp:TextBox ID="t3" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+        <td>product quantity</td>
+        <td><asp:TextBox ID="t4" runat="server"></asp:TextBox></td>
+        </tr>
+
+        <tr>
+        <td>product image</td>
+        <td><asp:FileUpload ID="f1" runat="server" /></td>
+        </tr>
+
+        <tr>
+        <td colspan="2" align="center">
+            <asp:Button ID="b1" runat="server" Text="upload" OnClick="b1_Click" />
+        </td>
+        </tr>
+    </table>
+</asp:Content>
--- WebApptest1/website/admin/add_product.aspx.cs
+++ WebApptest1/website/admin/add_product.aspx.cs
@@ -9,11 +9,11 @@ using System.Data.SqlClient;
 
 namespace WebApptest1.website.admin
 {
-    public partial class adminlogin : System.Web.UI.Page
+    public partial class add_product : System.Web.UI.Page
     {
-        //This database path have to be changed according to the environment
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nisal_Dilshan\Desktop\web api\ASP.net-webAPI\WebApptest1\App_Data\shopping.mdf;Integrated Security=True");
-        int i;
+        string a, b;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -21,28 +21,17 @@ namespace WebApptest1.website.admin
 
         protected void b1_Click(object sender, EventArgs e)
         {
-            i = 0;
+            a = Class1.GetRandomPassword(10).ToString();
+            f1.SaveAs(Request.PhysicalApplicationPath + "/website/images/" + a + f1.FileName.ToString());
+            b = "/website/images/" + a + f1.FileName.ToString();
+
             con.Open();
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from admin_login where username='"+t1.Text+"' and password = '"+t2.Text+"'";
+            cmd.CommandText = "insert into product values('"+t1.Text+"','"+t2.Text+"',"+t3.Text+","+t4.Text+",'"+b.ToString()+"')";
             cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            i = Convert.ToInt32(dt.Rows.Count.ToString());
-
-            if(i == 1)
-            {
-                Response.Redirect("testing.aspx");
-            }
-
-            else
-            {
-                l1.Text = "--  Invalid Username or Password";
-            }
-
             con.Close();
+            
         }
     }
 }
