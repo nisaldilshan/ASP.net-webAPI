3d116b45b76918c99f0247247bd3e63bde6443a8
Nisal Dilshan <bcnisal@gmail.com>
2018-06-06 01:10:30 +0530

    user side item view

---
+++ WebApptest1/website/user/display_item.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/website/user/user.Master" AutoEventWireup="true" CodeBehind="display_item.aspx.cs" Inherits="WebApptest1.website.user.display_item" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="c1" runat="server">
+    <asp:Repeater ID="d1" runat="server">
+        <HeaderTemplate>
+            <ul>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <ul>
+                <li class="last">
+                    <a href="#"><img src="<%#Eval("product_images") %>"" alt=""</a>
+                    <div class="product-info">
+                        <h3> <%#Eval("product_name") %> </h3>
+                        <div class="product-desc">
+                            <h4><%#Eval("product_qty") %> Available</h4>
+                            <p><%#Eval("product_desc") %></p>
+                            <strong class="price">$<%#Eval("product_price") %></strong>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </ItemTemplate>
+        <FooterTemplate>
+            </ul> 
+        </FooterTemplate>
+    </asp:Repeater>
+
+</asp:Content>
--- WebApptest1/website/user/display_item.aspx.cs
+++ WebApptest1/website/user/display_item.aspx.cs
@@ -7,32 +7,26 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace WebApptest1.website.admin
+namespace WebApptest1.website.user
 {
-    public partial class add_product : System.Web.UI.Page
+    public partial class display_item : System.Web.UI.Page
     {
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nisal_Dilshan\Desktop\web api\ASP.net-webAPI\WebApptest1\App_Data\shopping.mdf;Integrated Security=True");
-        string a, b;
-
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void b1_Click(object sender, EventArgs e)
-        {
-            a = Class1.GetRandomPassword(10).ToString();
-            f1.SaveAs(Request.PhysicalApplicationPath + "/website/images/" + a + f1.FileName.ToString());
-            b = "/website/images/" + a + f1.FileName.ToString();
-
             con.Open();
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "insert into product values('"+t1.Text+"','"+t2.Text+"',"+t3.Text+","+t4.Text+",'"+b.ToString()+"')";
+            cmd.CommandText = "select * from product";
             cmd.ExecuteNonQuery();
 
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            d1.DataSource = dt;
+            d1.DataBind();
             con.Close();
-            
+
         }
     }
 }
