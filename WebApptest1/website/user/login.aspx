ac139344a900949196d4829c5b146ebd16cc19c4
Nisal Dilshan <bcnisal@gmail.com>
2018-06-08 23:06:44 +0530

    User Login page added

---
+++ WebApptest1/website/user/login.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/website/user/user.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="WebApptest1.website.user.login" %>
+
+<asp:Content Id="content1" ContentPlaceHolderID="c1" runat="server">
+
+    <table>
+
+        <tr>
+            <td>Email </td>
+            <td><asp:TextBox ID="emailBox" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Password </td>
+            <td><asp:TextBox ID="passwordBox" runat="server" TextMode="Password"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            </td>
+        </tr>
+
+    </table>
+
+</asp:Content>
---
+++ WebApptest1/website/user/login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApptest1.website.user
+{
+    public partial class login : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nisal_Dilshan\Desktop\web api\ASP.net-webAPI\WebApptest1\App_Data\shopping.mdf;Integrated Security=True");
+        int tot = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select* from UserRegister where email='"+emailBox.Text+"' and password='"+passwordBox.Text+"'";
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            tot = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (tot>0)
+            {
+                Response.Redirect("checkout.aspx");
+            }
+            else
+            {
+                Label1.Text = "Invalid Email or Password";
+            }
+
+            emailBox.Text = "";
+            passwordBox.Text = "";
+
+            con.Close();
+        }
+    }
+}
